44a5b8ad9f75ee279d541bbca8a8c222a3b337b6
Aurélio Vinícius <aurelio@alunos.utfpr.edu.br>
2018-05-02 18:07:33 -0300

    tentativa com hsl

--- src/main.c
+++ src/main.c
@@ -20,7 +20,8 @@ int main() {
 
 	char nome[30];
 
-	Imagem *in, *out, *buffer;
+	Imagem *in, *out, *buffer, *fundoAmpliado;
+	Imagem *fundo = abreImagem("fundo.bmp", 3);
 
 	//Laço para todas as imagens.
 	for(int cont = 0; cont < 9; cont += 1) {
@@ -29,11 +30,15 @@ int main() {
 		//Inicializa imagens auxiliares.
 		out 	= criaImagem(in->largura, in->altura, in->n_canais);
 		buffer 	= criaImagem(in->largura, in->altura, in->n_canais);
+		fundoAmpliado = criaImagem(in->largura, in->altura, in->n_canais);
+		copiaConteudo(in, buffer);
+		redimensionaNN(fundo, fundoAmpliado);
+		RGBParaHSL(in, in);		
 		
 		//Percorre cada pixel da imagem.
 		for(int y = 0; y < in->altura; y += 1) {
 			for(int x = 0; x < in->largura; x += 1) {
-				/* printf("Pixel %d\n", x * y);					
+				/* printf("Pixel %d\n", x * y);
 				printf("R: %.2f", in->dados[0][y][x]);
 				printf("G: %.2f", in->dados[1][y][x]);
 				printf("B: %.2f\n", in->dados[2][y][x]); */
@@ -51,7 +56,7 @@ int main() {
 					out->dados[2][y][x] = in->dados[2][y][x];
 				} */
 
-				if(in->dados[0][y][x] + in->dados[2][y][x] < in->dados[1][y][x]) {
+				/* if(in->dados[1][y][x] / (in->dados[0][y][x] + in->dados[2][y][x]) > 1) {
 					out->dados[0][y][x] = 0.0f;
 					out->dados[1][y][x] = 0.0f;
 					out->dados[2][y][x] = 0.0f;
@@ -60,10 +65,29 @@ int main() {
 					out->dados[0][y][x] = in->dados[0][y][x];
 					out->dados[1][y][x] = in->dados[1][y][x];
 					out->dados[2][y][x] = in->dados[2][y][x];
+				} */
+
+				if((in->dados[0][y][x] > 65 && in->dados[0][y][x] < 145) &&
+						(in->dados[1][y][x] > 0.25f) &&
+						(in->dados[2][y][x] > 0.15f && in->dados[2][y][x] < 0.85f)) {					
+					out->dados[0][y][x] = fundoAmpliado->dados[0][y][x];
+					out->dados[1][y][x] = fundoAmpliado->dados[1][y][x];
+					out->dados[2][y][x] = fundoAmpliado->dados[2][y][x];
 				}
+				else {
+					out->dados[0][y][x] = buffer->dados[0][y][x];
+					out->dados[1][y][x] = buffer->dados[1][y][x];
+					out->dados[2][y][x] = buffer->dados[2][y][x];	
+				}
+
+				/* printf("Pixel %d\n", x * y);
+				printf("H: %.2f", in->dados[0][y][x]);
+				printf("S: %.2f", in->dados[1][y][x]);
+				printf("L: %.2f\n", in->dados[2][y][x]); */
 				
 			}
-		}
+		}		
+
 		sprintf(nome, "../resultados/teste%d.bmp", cont);
 		salvaImagem(out, nome);
 	}
