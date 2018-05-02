180638bab98914cad7bec6fdaa1bb4c497a721a5
Aurélio Vinícius <aurelio@alunos.utfpr.edu.br>
2018-05-02 19:41:31 -0300

    nova tentativa

--- src/main.c
+++ src/main.c
@@ -33,11 +33,13 @@ int main() {
 		fundoAmpliado = criaImagem(in->largura, in->altura, in->n_canais);
 		copiaConteudo(in, buffer);
 		redimensionaNN(fundo, fundoAmpliado);
-		RGBParaHSL(in, in);		
+		RGBParaHSL(in, in);	
+
+		int flag = 1;	
 		
 		//Percorre cada pixel da imagem.
-		for(int y = 0; y < in->altura; y += 1) {
-			for(int x = 0; x < in->largura; x += 1) {
+		for(int y = 1; y < in->altura - 1; y += 1) {
+			for(int x = 1; x < in->largura - 1; x += 1) {
 				/* printf("Pixel %d\n", x * y);
 				printf("R: %.2f", in->dados[0][y][x]);
 				printf("G: %.2f", in->dados[1][y][x]);
@@ -65,19 +67,50 @@ int main() {
 					out->dados[0][y][x] = in->dados[0][y][x];
 					out->dados[1][y][x] = in->dados[1][y][x];
 					out->dados[2][y][x] = in->dados[2][y][x];
-				} */
+				} */								
 
-				if((in->dados[0][y][x] > 65 && in->dados[0][y][x] < 145) &&
-						(in->dados[1][y][x] > 0.25f) &&
-						(in->dados[2][y][x] > 0.15f && in->dados[2][y][x] < 0.85f)) {					
+				int verde = 0;
+				int cor	  = 0;
+
+				float somatorioR = 0;
+				float somatorioG = 0;
+				float somatorioB = 0;
+
+
+				for(int j = y - 1; j <= y + 1; j += 1) {
+					for(int i = x - 1; i <= x + 1; i += 1) {						
+						if(!((in->dados[0][j][i] > 65 && in->dados[0][j][i] < 145) &&
+							(in->dados[1][j][i] > 0.25f) &&
+							(in->dados[2][j][i] > 0.15f && in->dados[2][j][i] < 0.85f))) {
+							cor += 1;
+							somatorioR += buffer->dados[0][j][i];
+							somatorioG += buffer->dados[1][j][i];
+							somatorioB += buffer->dados[2][j][i];
+						}
+						else {							
+							verde += 1;
+							somatorioR += fundoAmpliado->dados[0][j][i];
+							somatorioG += fundoAmpliado->dados[1][j][i];
+							somatorioB += fundoAmpliado->dados[2][j][i];
+						}
+					}		
+				}
+				
+				
+				if (verde == 9) {
 					out->dados[0][y][x] = fundoAmpliado->dados[0][y][x];
 					out->dados[1][y][x] = fundoAmpliado->dados[1][y][x];
-					out->dados[2][y][x] = fundoAmpliado->dados[2][y][x];
+					out->dados[2][y][x] = fundoAmpliado->dados[2][y][x];					
 				}
-				else {
+				else if(cor == 9){
 					out->dados[0][y][x] = buffer->dados[0][y][x];
 					out->dados[1][y][x] = buffer->dados[1][y][x];
-					out->dados[2][y][x] = buffer->dados[2][y][x];	
+					out->dados[2][y][x] = buffer->dados[2][y][x];					
+				}
+				else {
+					out->dados[0][y][x] = somatorioR/9;
+					out->dados[1][y][x] = somatorioG/9;
+					out->dados[2][y][x] = somatorioB/9;
 				}
 
 				/* printf("Pixel %d\n", x * y);
