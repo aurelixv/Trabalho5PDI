ce80b0a602088e3c8f335e305b521669956d6186
Lincoln <linkskill@gmail.com>
2018-05-12 16:21:56 -0300

    fix

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "pdi.h"
 
 void verdeParaCinza(Imagem *in, Imagem *cinza);
@@ -124,18 +125,19 @@ void atribuiFundo(Imagem *in, Imagem *buffer, Imagem *fundoAmpliado, Imagem *out
 void verdeParaCinza(Imagem *in, Imagem *cinza){
 	for(int y = 0; y < in->altura; y += 1) {
 		for(int x = 0; x < in->largura; x += 1) {
-			if((in->dados[0][y][x] > 70 && in->dados[0][y][x] < 140) 
+			if((in->dados[0][y][x] > 65 && in->dados[0][y][x] < 145) 
 					&& (in->dados[1][y][x] > 0.25f) 
 					&& (in->dados[2][y][x] > 0.15f && in->dados[2][y][x] < 0.715f))
-				cinza->dados[0][y][x] = in->dados[0][y][x]/(105 + abs(105 - in->dados[0][y][x]));
-			else if((in->dados[0][y][x] > 95 && in->dados[0][y][x] < 125) 
-					&& (in->dados[1][y][x] > 0.45f)
-					&& (in->dados[2][y][x] >= 0.6f && in->dados[2][y][x] < 0.85f))
-				cinza->dados[0][y][x] = (in->dados[0][y][x] - abs(105 - in->dados[0][y][x]))
-						/(105 + abs(105 - in->dados[0][y][x]));
+				cinza->dados[0][y][x] = fabs((in->dados[0][y][x] - abs(105 - in->dados[0][y][x]))
+						/(105 + abs(105 - in->dados[0][y][x])));
+			else if((in->dados[0][y][x] > 90 && in->dados[0][y][x] < 120) 
+					&& (in->dados[1][y][x] > 0.5f)
+					&& (in->dados[2][y][x] > 0.715f && in->dados[2][y][x] < 0.85f))
+				cinza->dados[0][y][x] = fabs((in->dados[0][y][x] - abs(105 - in->dados[0][y][x]))
+						/(105 + abs(105 - in->dados[0][y][x])));
 			else
-				cinza->dados[0][y][x] = (in->dados[0][y][x] - abs(105 - in->dados[0][y][x]))
-						/(210 + abs(105 - in->dados[0][y][x]));
+				cinza->dados[0][y][x] = fabs((in->dados[0][y][x] - abs(105 - in->dados[0][y][x]))
+						/(255 + abs(105 - in->dados[0][y][x])));
 		}
 	}	
 }
@@ -143,7 +145,7 @@ void verdeParaCinza(Imagem *in, Imagem *cinza){
 void atribuiFundoCinza(Imagem *cinza, Imagem *buffer, Imagem *fundoAmpliado, Imagem *saida){
 	for(int y = 0; y < cinza->altura; y += 1) {
 		for(int x = 0; x < cinza->largura; x += 1) {
-			if(cinza->dados[0][y][x] >= 0.8f) {
+			if(cinza->dados[0][y][x] > 0.5f) {
 				saida->dados[0][y][x] = fundoAmpliado->dados[0][y][x];
 				saida->dados[1][y][x] = fundoAmpliado->dados[1][y][x];
 				saida->dados[2][y][x] = fundoAmpliado->dados[2][y][x];
