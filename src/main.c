5d08349bcbeb3fa77a0d21f3ea631f1dafeb0391
Lincoln <linkskill@gmail.com>
2018-05-13 13:04:10 -0300

    fix

--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 #include "pdi.h"
 
 void verdeParaCinza(Imagem *in, Imagem *cinza);
-void atribuiFundo(Imagem *in, Imagem *buffer, Imagem *fundoAmpliado, Imagem *out);
 void atribuiFundoCinza(Imagem *cinza, Imagem *buffer, Imagem *fundoAmpliado, Imagem *saida);
 
 int main() {
@@ -23,8 +22,8 @@ int main() {
 
 	char nome[30];
 
-	Imagem *in, *out, *buffer, *fundoAmpliado;
-	Imagem *cinza, *saida ,*temp;
+	Imagem *in, *buffer, *fundoAmpliado;
+	Imagem *cinza, *saida;
 	Imagem *fundo = abreImagem("fundo.bmp", 3);
 
 	//Laço para todas as imagens.
@@ -32,10 +31,8 @@ int main() {
 		//Abre a imagem da vez em 3 canais (RGB).
 		in 		= abreImagem(imagens[cont], 3);
 		//Inicializa imagens auxiliares.
-		out 	= criaImagem(in->largura, in->altura, in->n_canais);
 		buffer 	= criaImagem(in->largura, in->altura, in->n_canais);
 		cinza 	= criaImagem(in->largura, in->altura, 1);
-		temp 	= criaImagem(in->largura, in->altura, 1);
 		fundoAmpliado = criaImagem(in->largura, in->altura, in->n_canais);
 		saida 	= criaImagem(in->largura, in->altura, in->n_canais);
 				
@@ -46,80 +43,23 @@ int main() {
 
 		verdeParaCinza(in, cinza);
 
-		sprintf(nome, "../resultados/cinza%d.bmp", cont);
+		sprintf(nome, "../resultados/%dcinza.bmp", cont);
 		salvaImagem(cinza, nome);
 
 		atribuiFundoCinza(cinza, buffer, fundoAmpliado, saida);
 		
-		sprintf(nome, "../resultados/cinzaTeste%d.bmp", cont);
+		sprintf(nome, "../resultados/%dcinzaTeste.bmp", cont);
 		salvaImagem(saida, nome);
-
-		//Percorre cada pixel da imagem.
-		atribuiFundo(in, buffer, fundoAmpliado, out);
-
-		sprintf(nome, "../resultados/teste%d.bmp", cont);
-		salvaImagem(out, nome);
 	}
 
 	destroiImagem(in);
-	destroiImagem(out);
 	destroiImagem(buffer);
 	destroiImagem(fundo);
 	destroiImagem(fundoAmpliado);
 	destroiImagem(cinza);
-	destroiImagem(temp);
 	destroiImagem(saida);
 
 	return 0;
-}
-
-void atribuiFundo(Imagem *in, Imagem *buffer, Imagem *fundoAmpliado, Imagem *out){
-	int verde, cor;
-	float somatorioR, somatorioG, somatorioB;
-
-	for(int y = 1; y < in->altura - 1; y += 1) {
-		for(int x = 1; x < in->largura - 1; x += 1) {								
-			verde = 0;
-			cor	  = 0;	
-			somatorioR = 0;
-			somatorioG = 0;
-			somatorioB = 0;
-			for(int j = y - 1; j <= y + 1; j += 1) {
-				for(int i = x - 1; i <= x + 1; i += 1) {						
-					if(!((in->dados[0][j][i] > 65 && in->dados[0][j][i] < 145) 
-							&& (in->dados[1][j][i] > 0.25f) 
-							&& (in->dados[2][j][i] > 0.15f && in->dados[2][j][i] < 0.715f))) {
-						cor += 1;
-						somatorioR += buffer->dados[0][j][i];
-						somatorioG += buffer->dados[1][j][i];
-						somatorioB += buffer->dados[2][j][i];
-					}
-					else {							
-						verde += 1;
-						somatorioR += fundoAmpliado->dados[0][j][i];
-						somatorioG += fundoAmpliado->dados[1][j][i];
-						somatorioB += fundoAmpliado->dados[2][j][i];
-					}
-				}		
-			}
-			
-			if (verde == 9) {
-				out->dados[0][y][x] = fundoAmpliado->dados[0][y][x];
-				out->dados[1][y][x] = fundoAmpliado->dados[1][y][x];
-				out->dados[2][y][x] = fundoAmpliado->dados[2][y][x];					
-			}
-			else if(cor == 9){
-				out->dados[0][y][x] = buffer->dados[0][y][x];
-				out->dados[1][y][x] = buffer->dados[1][y][x];
-				out->dados[2][y][x] = buffer->dados[2][y][x];					
-			}
-			else {
-				out->dados[0][y][x] = somatorioR/9;
-				out->dados[1][y][x] = somatorioG/9;
-				out->dados[2][y][x] = somatorioB/9;
-			}
-		}
-	}
 }
 
 void verdeParaCinza(Imagem *in, Imagem *cinza){
