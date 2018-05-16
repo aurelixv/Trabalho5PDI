ac5595426be09976824eeb91b69366361bee8631
Aurélio Vinícius <aurelio@alunos.utfpr.edu.br>
2018-05-16 14:02:20 -0300

    final

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 /** Declaração das novas funções **/
 void verdeParaCinza(Imagem *in, Imagem *cinza);
 void atribuiFundoCinza(Imagem *cinza, Imagem *buffer, Imagem *fundoAmpliado, Imagem *saida);
+void atribuiFundoCinzaBordaBorrada (Imagem *cinza, Imagem *buffer, Imagem *fundoAmpliado, Imagem *saida);
 
 int main() {
 	//Todas as imagens que serão utilizadas.
@@ -29,9 +30,13 @@ int main() {
 	Imagem *fundo = abreImagem("fundo.bmp", 3);
 
 	//Laço para todas as imagens.
-	for(int cont = 0; cont < 9; cont += 1) {
+	for(int cont = 1; cont <= 9; cont += 1) {
 		//Abre a imagem da vez em 3 canais (RGB).
-		in 		= abreImagem(imagens[cont], 3);
+		in 		= abreImagem(imagens[cont - 1], 3);
+
+		sprintf(nome, "../resultados/%d1original.bmp", cont);
+		salvaImagem(in, nome);
+
 		//Inicializa imagens auxiliares.
 		buffer 	= criaImagem(in->largura, in->altura, in->n_canais);
 		cinza 	= criaImagem(in->largura, in->altura, 1);
@@ -52,7 +57,7 @@ int main() {
 		verdeParaCinza(in, cinza);
 
 		//Imprime a imagem "mapa"
-		sprintf(nome, "../resultados/%dcinza.bmp", cont);
+		sprintf(nome, "../resultados/%d2cinza.bmp", cont);
 		salvaImagem(cinza, nome);
 
 		//Atribui a imagem final em saida, de acordo com o mapa
@@ -60,7 +65,15 @@ int main() {
 		atribuiFundoCinza(cinza, buffer, fundoAmpliado, saida);
 		
 		//Imprime a imagem de saida
-		sprintf(nome, "../resultados/%dcinzaTeste.bmp", cont);
+		sprintf(nome, "../resultados/%d3chroma.bmp", cont);
+		salvaImagem(saida, nome);
+
+		//Atribui a imagem final em saida, de acordo com o mapa
+		//Se a tonalidade cinza for claro é verde, então recebe fundo
+		atribuiFundoCinzaBordaBorrada(cinza, buffer, fundoAmpliado, saida);
+		
+		//Imprime a imagem de saida
+		sprintf(nome, "../resultados/%d4borrado.bmp", cont);
 		salvaImagem(saida, nome);
 	}
 
@@ -112,4 +125,53 @@ void atribuiFundoCinza(Imagem *cinza, Imagem *buffer, Imagem *fundoAmpliado, Ima
 			}		
 		}
 	}		
+}
+
+void atribuiFundoCinzaBordaBorrada (Imagem *cinza, Imagem *buffer, Imagem *fundoAmpliado, Imagem *saida) {
+	int cor = 0;
+	int verde = 0;
+	float somatorioR = 0;
+	float somatorioG = 0;
+	float somatorioB = 0;
+
+	for(int y = 1; y < cinza->altura - 1; y += 1) {
+		for(int x = 1; x < cinza->largura - 1; x += 1) {
+			cor = 0;
+			verde = 0;
+			somatorioR = 0;
+			somatorioG = 0;
+			somatorioB = 0;
+			for(int j = y - 1; j <= y + 1; j += 1) {
+				for(int i = x - 1; i <= x + 1; i += 1) {
+					if(cinza->dados[0][j][i] > 0.5f) {
+						verde += 1;
+						somatorioR += fundoAmpliado->dados[0][j][i];
+						somatorioG += fundoAmpliado->dados[1][j][i];
+						somatorioB += fundoAmpliado->dados[2][j][i];
+					}
+					else {
+						cor += 1;
+						somatorioR += buffer->dados[0][j][i];
+						somatorioG += buffer->dados[1][j][i];
+						somatorioB += buffer->dados[2][j][i];						
+					}
+				}
+			}
+			if (verde == 9) {
+				saida->dados[0][y][x] = fundoAmpliado->dados[0][y][x];
+				saida->dados[1][y][x] = fundoAmpliado->dados[1][y][x];
+				saida->dados[2][y][x] = fundoAmpliado->dados[2][y][x];
+			}
+			else if(cor == 9){
+				saida->dados[0][y][x] = buffer->dados[0][y][x];
+				saida->dados[1][y][x] = buffer->dados[1][y][x];
+				saida->dados[2][y][x] = buffer->dados[2][y][x];
+			}
+			else {				
+				saida->dados[0][y][x] = somatorioR/9;
+				saida->dados[1][y][x] = somatorioG/9;
+				saida->dados[2][y][x] = somatorioB/9;
+			}
+		}
+	}
 }
